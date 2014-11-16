5ab564c295a1cda0c391158cbaa8b905135162e1
Ian Macalinao <ian@giza.us>
2014-11-16 07:14:06 -0600

    add console log

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -54,6 +54,7 @@ angular.module('hottrade', ['gajus.swing', 'ui.router'])
   $scope.kimonoInfo = [];
   $http.jsonp('https://www.kimonolabs.com/api/27zrivbo?apikey=sNaDL4glHt0HIxcbjOTvflpT3ppp85WI&callback=JSON_CALLBACK').success(function(data) {
     $scope.kimonoInfo = data;
+    console.log(data);
   });
 
   $scope.passed = [];
