91008dfbdef9eb4cc3da72729fb2555a5c04faa1
Ian Macalinao <ian@giza.us>
2014-11-16 00:17:44 -0600

    begin redoing styles

---
+++ src/main/resources/public/js/app.js
@@ -0,0 +1,11 @@
+var auth = 'Basic WjhlYWYyam1ZOEY1TG90WnJaZHQxbVlKdUVWUStuN1hyckUzL3RTT05tODpaOGVhZjJqbVk4RjVMb3RaclpkdDFtWUp1RVZRK243WHJyRTMvdFNPTm04';
+
+$('body').bind('touchmove', function(e) {
+  e.preventDefault()
+});
+
+angular.module('hottrade', ['gajus.swing', 'ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+});
